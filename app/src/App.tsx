3594ecdf436277737f6d4e9121b6c9fcbb435fd5
Benjamin Okkema <ben@okkema.org>
2022-06-22 00:17:42 -0500

    update line chart

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,30 +1,15 @@
-import { useEffect, useState } from "react";
 import LineChart from "./charts/LineChart"
 import useSensors from "./useSensors"
 
 function App() {
-  const raw = useSensors()
-  const json = JSON.stringify(raw, null, "\t")
-  const [data, setData] = useState()
-
-  useEffect(() => {
-    setData(data => [...data, raw])
-  }, [raw])
-
-  // const chartData = [];
-  //   for (let i = 0; i < 20; i++) {
-  //     const value = Math.floor(Math.random() * i + 3);
-  //     chartData.push({
-  //       label: i,
-  //       value,
-  //       tooltipContent: `<b>x: </b>${i}<br><b>y: </b>${value}`
-  //     });
-  //   }
+  const { chart, current } = useSensors()
+  const raw = JSON.stringify(current, null, "\t")
+  const stream = JSON.stringify(chart, null, "\t")
   
   return (
     <div>
-      <pre>{json}</pre>
-      <LineChart width={400} height={300} data={chartData} />
+      <pre>{raw}</pre>
+      <LineChart width={400} height={300} data={chart} />
     </div>
   )
 }
