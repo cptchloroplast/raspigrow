43ff70aa7949f221829b1661a1539ff3d3252b9e
Benjamin Okkema <ben@okkema.org>
2025-01-05 21:07:47 -0600

    wip: remove python files

--- server/src/redis.py
+++
@@ -1,9 +0,0 @@
-from redis.asyncio import Redis
-
-from src.settings import Settings
-
-
-class RedisFactory:
-    @staticmethod
-    def create(settings: Settings):
-        return Redis(host=settings.REDIS_HOSTNAME, decode_responses=True)
