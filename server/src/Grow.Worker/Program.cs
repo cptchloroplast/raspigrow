2b24830ce5cff03b1591aa78c6283f33b1e8994a
Benjamin Okkema <ben@okkema.org>
2025-01-05 21:30:06 -0600

    feat: add server worker

---
+++ server/src/Grow.Worker/Program.cs
@@ -0,0 +1,12 @@
+﻿IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+IHost host = Host.CreateDefaultBuilder(args)
+    // .ConfigureServices(services =>
+    // {
+    // })
+    .Build();
+var services = host.Services;
+await host.RunAsync();
