fa102e555d2afcd44e5d74b9505d418b72620fe2
Benjamin Okkema <ben@okkema.org>
2025-02-18 21:54:39 -0600

    feat: add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+locals {
+  secrets = {
+    "TF_API_TOKEN" : var.TF_API_TOKEN,
+    "NUGET_GITHUB_TOKEN" : var.NUGET_GITHUB_TOKEN,
+  }
+}
+
+module "secrets" {
+  for_each = local.secrets
+
+  source  = "app.terraform.io/okkema/secret/github"
+  version = "~> 0.2"
+
+  repository = var.github_repository
+  key        = each.key
+  value      = each.value
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "github_repository" {}
+
+# GitHub Actions Secrets
+variable "TF_API_TOKEN" {
+  sensitive = true
+}
+variable "NUGET_GITHUB_TOKEN" {
+  sensitive = true
+}
